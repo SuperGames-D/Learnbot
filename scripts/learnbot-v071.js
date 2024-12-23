65101cd1adfda6a2ebcdf1884fcacff450bb9054
SuperGames- <129550193+SuperGames-D@users.noreply.github.com>
2024-12-23 21:58:14 +0000

    Release model 0.71
    - You can now give feedback on alternate responses
    - Feedback has more context now
    - Bugfixes

--- scripts/learnbot-v071.js
+++ scripts/learnbot-v071.js
@@ -1,8 +1,8 @@
-// Imagebot v0.1
+// Learnbot v0.71
 
 
-const removeWords = ["what", "when", "who", "why", "how", "where", "does", "often", "much", "do", "many", "it", "about", "which", "define", "tell me", "can you explain", "draw a", "generate a", "create a", "generate", "draw", "create"];
-const botname = "Imagebot"
+const removeWords = ["what", "when", "who", "why", "how", "where", "does", "often", "much", "do", "many", "it", "about", "which", "define", "tell me", "can you explain"];
+const botname = "Learnbot"
 let database = []
 const punctuation = [".", "!", "?"]
 const punctuationAll = [".", "!", "?", ",", ";"]
@@ -11,12 +11,8 @@ let conversationAI = []
 let conversationUser = []
 let conversationAlternate = []
 let userFontSize = 15
-let imgMatrix = ""
-for (let i = 0; i <= 100; i++) {
-    imgMatrix += "1"
-}
 
-if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){
     document.getElementById("form").style.width = "100%"
     document.getElementById("form").style.left = "0%"
     document.getElementById("form").style.transform = "translateX(0%)"
@@ -33,23 +29,8 @@ if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(naviga
     document.getElementById("teachArea").style.height = "10%"
     document.getElementById("teachArea").style.width = "90%"
     userFontSize = 40
-}
+  }
 
-document.getElementById("teachArea").style.width = "100%"
-document.getElementById("teachArea").style.textAlign = "left"
-document.getElementById("teachArea").innerHTML = "<text style=\"font-size: 20px\">Teach:</text><br>"
-document.getElementById("teachArea").innerHTML += "<textarea style=\"display: none\" id=\"teach\"</textarea>"
-document.getElementById("teachArea").innerHTML += "<br>"
-for (let i = 0; i <= 100; i++) {
-    if (i != 0) {
-    document.getElementById("teachArea").innerHTML += "<button id=\"img_button_" + String(i) + "\" onclick=\"imgButtonChange(" + String(i) + ")\" style=\"width: 20px; height: 20px; margin: 1px; background-color: white; color: white; border: none\">aa</button>"
-    if (i % 10 == 0) {
-        document.getElementById("teachArea").innerHTML += "<br>"
-    }
-}
-}
-document.getElementById("teachArea").innerHTML += "<br>Name: <input id=\"imgName\">"
-document.getElementById("teachArea").innerHTML += "<br><button onclick=\"addImage()\" style=\"font-size: 20px; background-color: black; border: 2px solid white; color: white; padding-left: 30px; padding-right: 30px\">Add</button>"
 
 function splitText(text) {
     let sentences = [];
@@ -122,13 +103,13 @@ function askQuestion() {
                     math = math + String(a) + "+" + String(b) + " is " + String(a + b) + "."
                     math = math + String(a) + "-" + String(b) + " is " + String(a - b) + "."
                     math = math + String(a) + "*" + String(b) + " is " + String(a * b) + "."
-                    if (a / b == Math.round(a / b)) {
-                        math = math + String(a) + "/" + String(b) + " is " + String(Math.round(a / b)) + "."
+                    if (a / b == Math.round(a/b)) {
+                    math = math + String(a) + "/" + String(b) + " is " + String(Math.round(a / b)) + "."
                     }
                     else {
-                        math = math + String(a) + "/" + String(b) + " is approximately " + String(Math.round(a / b)) + "."
-                    }
-                }
+                    math = math + String(a) + "/" + String(b) + " is approximately " + String(Math.round(a / b)) + "."
+               } 
+            }
             }
             document.getElementById("teach").value += "." + math
             changeDatabase()
@@ -169,7 +150,7 @@ function askQuestion() {
             }
             ask = " " + ask
             for (let i in removeWords) {
-                ask = ask.replaceAll("" + removeWords[i], " ")
+                ask = ask.replaceAll(" " + removeWords[i], " ")
             }
             ask = ask.slice(1)
             let words = ask.split(/\s+/).map(word => word.replace(/[.,!?;:]/g, '').toLowerCase());
@@ -185,7 +166,7 @@ function askQuestion() {
                     }
                     if (synonyms.hasOwnProperty(keyword.toLowerCase())) {
                         if (sentence.toLowerCase().includes(synonyms[keyword.toLowerCase()][0]) || sentence.toLowerCase().includes(synonyms[keyword.toLowerCase()][1])) {
-                            load += 1
+                            load +=1
                         }
                     }
                 });
@@ -196,29 +177,10 @@ function askQuestion() {
             let answer = ""
             if (results.length != 0) {
                 for (let i in results) {
-                    answer = " " + results[i]
-                    for (let i in Object.keys(synonyms)) {
-                        if (Math.random() > 0.3) {
-                            answer = answer.replace(" " + Object.keys(synonyms)[i], " " + synonyms[Object.keys(synonyms)[i]][Math.round(Math.random())])
-                        }
-                    }
-                    answer = answer.replaceAll(/\[.*?\]/g, '');
-                    answer = replaceAllCaseInsensitive(answer, "i am", "you're")
-                    answer = replaceAllCaseInsensitive(answer, "you are", "i'm")
-                    answer = answer.slice(1);
-                    answer = answer.charAt(0).toUpperCase() + answer.slice(1);
-                    if (punctuation.includes(answer[answer.length - 1])) {
-                        answer = answer
-                    }
-                    else {
-                        answer = answer + "."
-                    }
-                    newResults.push(answer)
-                }
-                answer = " " + combineStrings(results)
-                for (let i in Object.keys(synonyms)) {
+                answer = " " + results[i]
+                for(let i in Object.keys(synonyms)) {
                     if (Math.random() > 0.3) {
-                        answer = answer.replace(" " + Object.keys(synonyms)[i], " " + synonyms[Object.keys(synonyms)[i]][Math.round(Math.random())])
+                    answer = answer.replace(" " + Object.keys(synonyms)[i], " " + synonyms[Object.keys(synonyms)[i]][Math.round(Math.random())])
                     }
                 }
                 answer = answer.replaceAll(/\[.*?\]/g, '');
@@ -232,7 +194,26 @@ function askQuestion() {
                 else {
                     answer = answer + "."
                 }
+                newResults.push(answer)
             }
+            answer = " " + results[0]
+            for(let i in Object.keys(synonyms)) {
+                if (Math.random() > 0.3) {
+                answer = answer.replace(" " + Object.keys(synonyms)[i], " " + synonyms[Object.keys(synonyms)[i]][Math.round(Math.random())])
+                }
+            }
+            answer = answer.replaceAll(/\[.*?\]/g, '');
+            answer = replaceAllCaseInsensitive(answer, "i am", "you're")
+            answer = replaceAllCaseInsensitive(answer, "you are", "i'm")
+            answer = answer.slice(1);
+            answer = answer.charAt(0).toUpperCase() + answer.slice(1);
+            if (punctuation.includes(answer[answer.length - 1])) {
+                answer = answer
+            }
+            else {
+                answer = answer + "."
+            }
+        }
             let newline = ""
             if (z != 0) {
                 newline = "<br><br>"
@@ -254,36 +235,16 @@ function askQuestion() {
 }
 
 function message(message, sender) {
-    document.getElementById("conversation").innerHTML += "<br><br><b style=\"font-size:" + String(userFontSize) + "px; text-align: left\">" + sender + "</b><br>"
-    if (sender == botname) {
-        document.getElementById("conversation").innerHTML += "<br>"
-    for (let i in message) {
-        if (i != 0) {
-        if (message[i] == "0") {
-            document.getElementById("conversation").innerHTML += "<div style=\"width: 20px; height: 20px; margin: 0px; background-color: black; color: black; display: inline\">aa</div>"
-        }
-        else if (message[i] == "1") {
-            document.getElementById("conversation").innerHTML += "<div style=\"height: 20px; width: 20px; margin: 0px; background-color: white; color: white; display: inline\">aa</div>"
-        }
-        if (i % 10 == 0) {
-            document.getElementById("conversation").innerHTML += "<br>"
-        }
-    }
-    }}
-    else {
-        document.getElementById("conversation").innerHTML += message
-    }
+    document.getElementById("conversation").innerHTML += "<br><br><b style=\"font-size:" + String(userFontSize) + "px; text-align: left\">" + sender + "</b><br>" + message;
     if (sender == botname) {
         conversationAI.push(message)
-        /*
-        document.getElementById("conversation").innerHTML += "<br><button onclick=\"goodResponse(" + String(conversationAI.length - 1) + ")\" class=\"responseButton\"\"><img src=\"images/like.png\" style=\"transform: scaleY(1);\" height=\"" + String(userFontSize * 1.3) + "px\"></button>"
-        document.getElementById("conversation").innerHTML += "<button onclick=\"badResponse(" + String(conversationUser.length - 1) + ")\" class=\"responseButton\"\"><img src=\"images/like.png\" style=\"transform: scaleY(-1);\" height=\"" + String(userFontSize * 1.3) + "px\"></button>"
-        document.getElementById("conversation").innerHTML += "<button onclick=\"readAloud(" + String(conversationAI.length - 1) + ")\" class=\"responseButton\"\"><img src=\"images/read-aloud.png\" style=\"transform: scaleY(1);\" height=\"" + String(userFontSize * 1.3) + "px\"></button>"
-        document.getElementById("conversation").innerHTML += "<button onclick=\"copy(" + String(conversationAI.length - 1) + ")\" class=\"responseButton\"\"><img src=\"images/copy.png\" style=\"transform: scaleY(1);\" height=\"" + String(userFontSize * 1.3) + "px\"></button>"
-        document.getElementById("conversation").innerHTML += "<button onclick=\"secondaryResults(" + String(conversationAI.length - 1) + ")\" id=\"secondary-button" + String(conversationAI.length - 1) + "\" class=\"responseButton\"\"><img src=\"images/secondary-answers.png\" style=\"transform: scaleY(1);\" height=\"" + String(userFontSize * 1.3) + "px\"></button>"
-        document.getElementById("conversation").innerHTML += "<div id=\"secondary-answers" + String(conversationAI.length - 1) + "\"></div>"
-  */
-        }
+        document.getElementById("conversation").innerHTML += "<br><button onclick=\"goodResponse(" + String(conversationAI.length-1) +")\" class=\"responseButton\"\"><img src=\"images/like.png\" style=\"transform: scaleY(1);\" height=\"" + String(userFontSize*1.3) +"px\"></button>"
+        document.getElementById("conversation").innerHTML += "<button onclick=\"badResponse(" + String(conversationUser.length-1) +")\" class=\"responseButton\"\"><img src=\"images/like.png\" style=\"transform: scaleY(-1);\" height=\"" + String(userFontSize*1.3) +"px\"></button>"
+        document.getElementById("conversation").innerHTML += "<button onclick=\"readAloud(" + String(conversationAI.length-1) +")\" class=\"responseButton\"\"><img src=\"images/read-aloud.png\" style=\"transform: scaleY(1);\" height=\"" + String(userFontSize*1.3) +"px\"></button>"
+        document.getElementById("conversation").innerHTML += "<button onclick=\"copy(" + String(conversationAI.length-1) +")\" class=\"responseButton\"\"><img src=\"images/copy.png\" style=\"transform: scaleY(1);\" height=\"" + String(userFontSize*1.3) +"px\"></button>"
+        document.getElementById("conversation").innerHTML += "<button onclick=\"secondaryResults(" + String(conversationAI.length-1) +")\" id=\"secondary-button" + String(conversationAI.length-1) +"\" class=\"responseButton\"\"><img src=\"images/secondary-answers.png\" style=\"transform: scaleY(1);\" height=\"" + String(userFontSize*1.3) +"px\"></button>"
+        document.getElementById("conversation").innerHTML += "<div id=\"secondary-answers" + String(conversationAI.length-1) +"\"></div>"
+    }
     else if (sender == "You") {
         conversationUser.push(message)
     }
@@ -301,8 +262,9 @@ function badResponse(q) {
     for (let i in punctuationAll) {
         userQ = userQ.replaceAll(punctuationAll[i], " ")
     }
+    userQ = " " + userQ
     for (let i in removeWords) {
-        userQ = userQ.replaceAll("" + removeWords[i], " ")
+        userQ = userQ.replaceAll(" " + removeWords[i], " ")
     }
     userQ = userQ.slice(1)
     console.log(userQ)
@@ -311,7 +273,18 @@ function badResponse(q) {
 }
 
 function goodResponse(msg) {
-    document.getElementById("teach").value = conversationAI[msg].replaceAll("<br>", "") + document.getElementById("teach").value
+    userQ = conversationUser[msg]
+    userQ = " " + userQ
+    for (let i in punctuationAll) {
+        userQ = userQ.replaceAll(punctuationAll[i], " ")
+    }
+    userQ = " " + userQ
+    for (let i in removeWords) {
+        userQ = userQ.replaceAll(" " + removeWords[i], " ")
+    }
+    userQ = userQ.slice(1)
+    console.log(userQ)
+    document.getElementById("teach").value = "[" + userQ + "]" + conversationAI[msg].replaceAll("<br>", "") + document.getElementById("teach").value
     changeDatabase()
 }
 
@@ -338,9 +311,9 @@ function secondaryResults(msg) {
         altResults.push([])
         for (let n in conversationAlternate[msg][i]) {
             if (n > 0) {
-                altResults[i].push(conversationAlternate[msg][i][n])
+        altResults[i].push(conversationAlternate[msg][i][n])
             }
-        }
+    }
     }
     for (let i in altResults[0]) {
         let txt = ""
@@ -350,19 +323,19 @@ function secondaryResults(msg) {
         newAltResults.push(txt)
     }
     for (let i in newAltResults) {
-        resultsText += newAltResults[i] + "<br>"
+        resultsText += newAltResults[i] + "<button onclick=\"goodAlternate(" + String(conversationAI.length-1) +"," + String(i) +")\" class=\"responseButton\"\"><img src=\"images/like.png\" style=\"transform: scaleY(1);\" height=\"" + String(userFontSize*1.3) +"px\"></button><br>"
     }
     if (resultsText == "<b>Secondary answers:</b><br>") {
         resultsText = "<b>Secondary answers:</b><br>No secondary answers found."
     }
     if (document.getElementById("secondary-answers" + String(msg)).innerHTML == "") {
-        document.getElementById("secondary-answers" + String(msg)).innerHTML = resultsText
-        document.getElementById("secondary-button" + String(msg)).style.transform = "scaleY(-1)"
-    }
-    else {
-        document.getElementById("secondary-answers" + String(msg)).innerHTML = ""
-        document.getElementById("secondary-button" + String(msg)).style.transform = "scaleY(1)"
-    }
+    document.getElementById("secondary-answers" + String(msg)).innerHTML = resultsText
+    document.getElementById("secondary-button"+ String(msg)).style.transform = "scaleY(-1)"
+}
+else {
+    document.getElementById("secondary-answers" + String(msg)).innerHTML = ""
+    document.getElementById("secondary-button" + String(msg)).style.transform = "scaleY(1)"
+}
 }
 
 
@@ -373,72 +346,35 @@ function replaceAllCaseInsensitive(input, search, replacement) {
     return input.replace(regex, replacement);
 }
 
-
-function imgButtonChange(btn) {
-    if (imgMatrix[btn] == "0") {
-        imgMatrix = imgMatrix.replaceAt(btn, "1")
-        document.getElementById("img_button_" + String(btn)).style.backgroundColor = "white"
-        document.getElementById("img_button_" + String(btn)).style.color = "white"
-}
-else {
-    imgMatrix = imgMatrix.replaceAt(btn, "0")
-    document.getElementById("img_button_" + String(btn)).style.backgroundColor = "black"
-    document.getElementById("img_button_" + String(btn)).style.color = "black"
-}
-}
-
-String.prototype.replaceAt = function(index, replacement) {
-    return this.substring(0, index) + replacement + this.substring(index + replacement.length);
-}
-
-function addImage() {
-    if (imgMatrix.length < 101) {
-        while (imgMatrix.length < 101) {
-            imgMatrix += "1"
+function goodAlternate(msg, alt) {
+    userQ = conversationUser[msg]
+    userQ = " " + userQ
+    for (let i in punctuationAll) {
+        userQ = userQ.replaceAll(punctuationAll[i], " ")
+    }
+    userQ = " " + userQ
+    for (let i in removeWords) {
+        userQ = userQ.replaceAll(" " + removeWords[i], " ")
+    }
+    userQ = userQ.slice(1)
+    let altResults = []
+    let newAltResults = []
+    for (let i in conversationAlternate[msg]) {
+        altResults.push([])
+        for (let n in conversationAlternate[msg][i]) {
+            if (n > 0) {
+        altResults[i].push(conversationAlternate[msg][i][n])
+            }
+    }
+    }
+    for (let i in altResults[0]) {
+        let txt = ""
+        for (n in altResults) {
+            txt += " " + altResults[n][i]
         }
+        newAltResults.push(txt)
     }
-    else if (imgMatrix.length > 101) {
-        while (imgMatrix.length > 101) {
-            imgMatrix = imgMatrix.substring(0, str.length - 1);
-        }
-    }
-    database.push(document.getElementById("teach").value + "[" + document.getElementById("imgName").value + "]" + imgMatrix + ".")
-    document.getElementById("imgName").value = ""
-    imgMatrix = ""
-    for (let i = 0; i < 100; i++) {
-        imgMatrix += "1"
-    }
-    for (let i = 0; i <= 100; i++) {
-        if (i != 0) {
-        document.getElementById("img_button_" + String(i)).style = "width: 20px; height: 20px; margin: 1px; background-color: white; color: white; border: none"
-        if (i % 10 == 0) {
-            document.getElementById("teachArea").innerHTML += "<br>"
-        }
-    }
-    }
+    console.log(newAltResults)
+    document.getElementById("teach").value = "[" + userQ + "]" + newAltResults[alt].replaceAll("<br>", "") + document.getElementById("teach").value
+    changeDatabase()
 }
-
-function combineStrings(strings) {
-    const maxLength = Math.max(...strings.map(str => str.length));
-    let result = '';
-    for (let i = 0; i < maxLength; i++) {
-      const charCount = {};
-  
-      for (const str of strings) {
-        if (str[i] !== undefined) {
-          const char = str[i];
-          charCount[char] = (charCount[char] || 0) + 1;
-        }
-      }
-      let maxChar = '';
-      let maxCount = 0;
-      for (const [char, count] of Object.entries(charCount)) {
-        if (count > maxCount) {
-          maxChar = char;
-          maxCount = count;
-        }
-      }
-      result += maxChar;
-    }
-    return result;
-  }
